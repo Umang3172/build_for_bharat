3bd2aee16c0970d6459797383257be92a80709ea
Rajkamal <rajkamal.singh1011@gmail.com>
2024-02-06 19:35:00 +0530

    pushed AR related files

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,37 @@
+import 'package:build_for_bharat/ARTryOn.dart';
 import 'package:build_for_bharat/modules/home/ui/home_screen.dart';
 import 'package:build_for_bharat/utils/screen_util.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
+// void main() {
+//   runApp(const MyApp());
+// }
+//
+// class MyApp extends StatelessWidget {
+//   const MyApp({super.key});
+//
+//   // This widget is the root of your application.
+//   @override
+//   Widget build(BuildContext context) {
+//     ScreenUtil(context);
+//     return const MaterialApp(
+//       debugShowCheckedModeBanner: false,
+//       // home: HomeScreen(),
+//       home: MyButton(),
+//     );
+//   }
+// }
+
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    ScreenUtil(context);
-    return const MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: HomeScreen(),
+    return MaterialApp(
+      home: LocalAndWebObjectsWidget()
     );
   }
 }
